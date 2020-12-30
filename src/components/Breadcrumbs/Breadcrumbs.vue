e082b2fc219fd8aa16c298f89da61c63757d0915
ths <972784674@qq.com>
2020-12-30 19:53:02 +0800

    原面包屑动画执行时在部分平板上会撑开 toolbar，改用新动画

--- src/components/Breadcrumbs/Breadcrumbs.vue
+++ src/components/Breadcrumbs/Breadcrumbs.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="flex">
-  <q-breadcrumbs style="margin-left: 10px;" active-color="none">
-    <transition-group name="breadcrumb">
-    <template v-for="(v,i) in breadcrumbs">
-     <q-breadcrumbs-el v-if="v.title" style="margin-top: 4px"
-       :label="v.title"
-       :icon="v.icon"
-       :key="i+v.title">
-         <div v-if="breadcrumbs.length !== i+1" style="margin:0px 8px 0px 8px">
-           /
-         </div>
-     </q-breadcrumbs-el>
-    </template>
-    </transition-group>
-  </q-breadcrumbs>
+    <q-breadcrumbs style="transform: translateX(10px);display: inline-block;" active-color="none">
+      <transition-group name="breadcrumb">
+        <template v-for="(v,i) in breadcrumbs">
+          <q-breadcrumbs-el v-if="v.title" style="line-height: 20px;margin-top: 6px;"
+                            :label="v.title"
+                            :icon="v.icon"
+                            :key="i+v.title">
+            <div v-if="breadcrumbs.length !== i+1" style="margin:0px 8px 0px 8px">
+              /
+            </div>
+          </q-breadcrumbs-el>
+        </template>
+      </transition-group>
+    </q-breadcrumbs>
   </div>
 </template>
 
