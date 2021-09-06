53f3c7ad096f69c5b3bd54a168ef11a12f7ec18c
ths <972784674@qq.com>
2021-09-06 16:09:51 +0800

    fix: 特殊分辨率下侧边栏会出现原生滚动条

--- src/components/Menu/BaseMenuItem.vue
+++ src/components/Menu/BaseMenuItem.vue
@@ -11,24 +11,24 @@
     <template v-for="(item,index) in myRouter">
       <template v-if="item.meta.isHidden !== true">
         <q-item-label v-if="item.meta.itemLabel"
-           header
-           class="text-weight-bold text-uppercase"
-           :key="item.meta.itemLabel">
+                      header
+                      class="text-weight-bold text-uppercase"
+                      :key="item.meta.itemLabel">
           {{item.meta.itemLabel}}
         </q-item-label>
 
         <!-- 没有孩子 -->
         <q-item v-if="!item.children"
-           clickable
-           v-ripple
-           :key="index"
-           :exact="item.path === '/'"
-           :class="baseItemClass"
-           :inset-level="initLevel"
-           :style="isWeChart?' line-height: normal':''"
-           active-class="baseItemActive"
-           :to="handleLink(basePath, item.path)"
-           @click="externalLink(basePath, item.path)"
+                clickable
+                v-ripple
+                :key="index"
+                :exact="item.path === '/'"
+                :class="baseItemClass"
+                :inset-level="initLevel"
+                :style="isWeChart?' line-height: normal':''"
+                active-class="baseItemActive"
+                :to="handleLink(basePath, item.path)"
+                @click="externalLink(basePath, item.path)"
         >
           <q-item-section avatar>
             <q-icon :name="item.meta.icon" />
@@ -40,14 +40,14 @@
 
         <!-- 有孩子 -->
         <q-expansion-item v-else
-           :duration="duration"
-           :class="baseItemClassWithNoChildren(item.path)"
-           :default-opened="item.meta.isOpen"
-           :header-inset-level="initLevel"
-           :key="index"
-           :icon="item.meta.icon"
-           :label="item.meta.title"
-           :style="isWeChart?' line-height: normal':''"
+                          :duration="duration"
+                          :class="baseItemClassWithNoChildren(item.path)"
+                          :default-opened="item.meta.isOpen"
+                          :header-inset-level="initLevel"
+                          :key="index"
+                          :icon="item.meta.icon"
+                          :label="item.meta.title"
+                          :style="isWeChart?' line-height: normal':''"
         >
 
           <!-- 菜单项缩进 + 0.2 ; 背景色深度 + 1 ; 如果上级菜单路径存在，则拼接上级菜单路径 -->
@@ -67,68 +67,68 @@
 </template>
 
 <script>
-export default {
-  name: 'base-menu-item',
-  data () {
-    return {
-      baseItemClass: this.bgColor + '-' + this.bgColorLevel + ' base-menu-item'
-    }
-  },
-  computed: {
-    /**
-     * 处理子菜单被激活的样式，同时修改父菜单样式
-     */
-    baseItemClassWithNoChildren () {
-      return (path) => {
-        return this.$route.fullPath.startsWith(path) ? 'baseRootItemActive base-menu-item' + this.baseItemClass : this.baseItemClass
+  export default {
+    name: 'base-menu-item',
+    data () {
+      return {
+        baseItemClass: this.bgColor + '-' + this.bgColorLevel + ' base-menu-item'
       }
     },
+    computed: {
+      /**
+       * 处理子菜单被激活的样式，同时修改父菜单样式
+       */
+      baseItemClassWithNoChildren () {
+        return (path) => {
+          return this.$route.fullPath.startsWith(path) ? 'baseRootItemActive base-menu-item' + this.baseItemClass : this.baseItemClass
+        }
+      },
 
-    /**
-     * 如果是微信浏览器，则添加 line-height: normal 样式
-     * @returns {boolean}
-     */
-    isWeChart () {
-      return navigator.userAgent.toLowerCase().indexOf('micromessenger') !== -1
-    }
-
-  },
-  props: ['myRouter', 'initLevel', 'bgColor', 'bgColorLevel', 'duration', 'basePath'],
-  methods: {
-
-    /**
-     * 处理内部链接
-     * @param basePath
-     * @param itemPath
-     */
-    handleLink (basePath, itemPath) {
-      const link = basePath === undefined ? itemPath : basePath + '/' + itemPath
-      if (link.indexOf('http') !== -1) {
-        return '#'
+      /**
+       * 如果是微信浏览器，则添加 line-height: normal 样式
+       * @returns {boolean}
+       */
+      isWeChart () {
+        return navigator.userAgent.toLowerCase().indexOf('micromessenger') !== -1
       }
-      return link
+
     },
+    props: ['myRouter', 'initLevel', 'bgColor', 'bgColorLevel', 'duration', 'basePath'],
+    methods: {
 
-    /**
-     * 处理外部链接
-     * @param basePath
-     * @param itemPath
-     * @returns {boolean}
-     */
-    externalLink (basePath, itemPath) {
-      const link = basePath === undefined ? itemPath : basePath + '/' + itemPath
-      const i = link.indexOf('http')
-      if (i !== -1) {
-        const a = document.createElement('a')
-        a.setAttribute('href', link.slice(i))
-        a.setAttribute('target', '_blank')
-        a.click()
-        return false
+      /**
+       * 处理内部链接
+       * @param basePath
+       * @param itemPath
+       */
+      handleLink (basePath, itemPath) {
+        const link = basePath === undefined ? itemPath : basePath + '/' + itemPath
+        if (link.indexOf('http') !== -1) {
+          return '#'
+        }
+        return link
+      },
+
+      /**
+       * 处理外部链接
+       * @param basePath
+       * @param itemPath
+       * @returns {boolean}
+       */
+      externalLink (basePath, itemPath) {
+        const link = basePath === undefined ? itemPath : basePath + '/' + itemPath
+        const i = link.indexOf('http')
+        if (i !== -1) {
+          const a = document.createElement('a')
+          a.setAttribute('href', link.slice(i))
+          a.setAttribute('target', '_blank')
+          a.click()
+          return false
+        }
       }
-    }
 
+    }
   }
-}
 </script>
 <style lang="sass">
 
@@ -157,7 +157,7 @@ export default {
       width: 3px
       height: 100%
       background: $ACTIVE_COLOR !important
-      top: -0.5px
-      right: 0px
+      top: 0
+      right: 0
 
 </style>
